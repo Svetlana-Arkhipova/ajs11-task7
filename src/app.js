4d33cbc18c2cbb2788fec7e0181421f8a1295d8e
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-12-26 20:12:55 +0300

    условие 4 и более

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export default class Validator {
   validateUsername(name) {
-    if (/\d{4,}/g.test(name)) {
+    if (/\d{4}/g.test(name)) {
       return false;
     }
     return (/^[a-z][\w-]*[a-z]$/i.test(name));
